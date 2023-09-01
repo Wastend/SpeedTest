1597fd15c05d76efdf5adbd95f9ed3bf29a4f6a1
Wastend <73016518+Wastend@users.noreply.github.com>
2023-09-01 22:56:49 +0600

    connected to the API

--- src/app/configs/ReceiveData.js
+++ src/app/configs/ReceiveData.js
@@ -1,3 +1,22 @@
-function getData() {
+import axios from 'axios'
 
+export async function getDataOnSentence(number) {
+  try {
+    const response = await axios.get(`https://fish-text.ru/get?&type=sentence&number=${number}`)
+    return response.data.text
+    // console.log('Выводим по количеству предложений ' + number + ': ' + response.data.text)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getDataOnWords(number) {
+  try {
+    const response = await axios.get(`https://fish-text.ru/get?&type=sentence&number=${number}`)
+    const sentences = response.data.text.split(' ').slice(0, number)
+    return sentences.join(' ')
+    // console.log('Выводим по количеству слов ' + number + ': ' +sentences.join(' '))
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getDataOnSentence, getDataOnWords } from '../../app/configs/ReceiveData'
+import './MainPage.scss'
 
 const MainPage = () => {
+  const [text, setText] = useState('')
+
+  async function setTextForTest(action, number) {
+    if (action === 'sentence') {
+      setText(await getDataOnSentence(number))
+    }
+    else if (action === 'words') {
+      setText(await getDataOnWords(number))
+    }
+  }
+
   return (
-    <>MainPage</>
+    <div className='MainPage'>
+      <div className="MainPage__buttons">
+        <button onClick={() => setTextForTest('sentence', 1)}>getDataOnSentence</button>
+        <button onClick={() => setTextForTest('words', 10)}>getDataOnWords</button>
+      </div>
+      <p>{text}</p>
+    </div >
   )
 }
 
